1c2efe0f6690e1109e94a1aa14798a43aa686d4a
Ian <defdef7721@gmail.com>
2026-05-13 19:44:00 +1000

    added checkoutform

--- script/script.js
+++ script/script.js
@@ -20,25 +20,31 @@ function displayCart(){
     let emptyMessage = document.getElementById("empty-cart-message");
     let cartSection = document.getElementById("cart-section");
 
+    let subtotalElement = document.getElementById("subtotal");
+
     cartlist.innerHTML = "";
+    let subtotal = 0;
 
     if (cart.length == 0){
         emptyMessage.style.display = "block";
         cartSection.style.display = "none";
-    } else{
+    } else {
         emptyMessage.style.display = "none";
-        cartSection.style.display = "block";
+        cartSection.style.display = "block center-text";
     }
 
     cart.forEach((item, index) => {
+
+        subtotal += item.price;
         let li = document.createElement("li");
         li.className = "cart-item";
 
         li.innerHTML = `
             <a href="${item.link}" class = "card">
-                <div class = "card-content">
+                <div class = "card-content font-white">
                     <img src="${item.image}" alt = "Menuimg" class = "card-image">
                     <h3>${item.name}</h3>
+                    <p>$${item.price.toFixed(2)} </p>
                 </div>
             </a>
         `;
@@ -47,8 +53,12 @@ function displayCart(){
         removeBtn.textContent = "Remove";
         removeBtn.onclick = () => removeItem(index);
         li.appendChild(removeBtn);
+        
         cartlist.appendChild(li);
+
+        
     });
+    subtotalElement.textContent =  `${subtotal.toFixed(2)}` ;
 }
 
 
@@ -59,7 +69,49 @@ function removeItem(index){
     cart.splice(index, 1);
     localStorage.setItem("cart", JSON.stringify(cart));
     location.reload();
+
 }
+
+document.addEventListener("DOMContentLoaded", ()=>{
+    
+    const checkoutBtn = document.getElementById("checkoutBtn");
+    const cartSection = document.getElementById("cart-section");
+    const checkoutForm = document.getElementById("checkout-form");
+    const pageTitle = document.getElementById("page-title");
+    const yourTitle = document.getElementById("your-order");
+
+    pageTitle.style.display = "none";
+    checkoutForm.style.display = "none";
+
+    let onCheckoutPage = false;
+
+    checkoutBtn.onclick = () => {
+        if (!onCheckoutPage){
+            cartSection.style.display = "none";
+            checkoutForm.style.display = "block";
+            pageTitle.style.display = "none";
+            yourTitle.style.display = "block";
+
+            checkoutBtn.textContent = "Go Back";
+            onCheckoutPage = true;
+        } else {
+            cartSection.style.display = "block";
+            checkoutForm.style.display = "none";
+            pageTitle.style.display = "block";
+            yourTitle.style.display = "none";
+
+            checkoutBtn.textContent = "Proceed to Checkout";
+            onCheckoutPage = false;
+
+        }
+
+    confirmOrder.onclick = () => {
+        alert("ORDER SUCCESSFULLY RECEIVED!")
+    }
+        
+    };
+});
+
 
 
 
