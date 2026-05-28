aa35ed8c448db8d91835fc95dd0fad3ff420c2c7
Ian <defdef7721@gmail.com>
2026-05-28 19:54:54 +1000

    added payment details page

--- script/script.js
+++ script/script.js
@@ -99,13 +99,19 @@ document.addEventListener("DOMContentLoaded", ()=> {
     // Initialise constant variables based on element id
     const checkoutBtn = document.getElementById("checkoutBtn");
     const cartSection = document.getElementById("cart-section");
+
     const checkoutForm = document.getElementById("checkout-form");
     const pageTitle = document.getElementById("page-title");
     const yourTitle = document.getElementById("your-order");
+    const paymentButton = document.getElementById("paymentButton");
+
+    const confirmOrder = document.getElementById("confirmOrder");
+    const paymentInfo = document.getElementById("payment-info");
 
     // Hide checkout page.
     pageTitle.style.display = "none";
     checkoutForm.style.display = "none";
+    paymentInfo.style.display = "none";
 
     let onCheckoutPage = false;
 
@@ -114,6 +120,7 @@ document.addEventListener("DOMContentLoaded", ()=> {
         if (!onCheckoutPage){
             cartSection.style.display = "none";
             checkoutForm.style.display = "block";
+
             pageTitle.style.display = "block";
             yourTitle.style.display = "none";
 
@@ -123,25 +130,33 @@ document.addEventListener("DOMContentLoaded", ()=> {
         } else {
             cartSection.style.display = "block";
             checkoutForm.style.display = "none";
+            
             pageTitle.style.display = "none";
             yourTitle.style.display = "block";
+        
 
             checkoutBtn.textContent = "Proceed to Checkout";
             onCheckoutPage = false;
 
         }
+    };
 
     confirmOrder.onclick = () => {
+
+        checkoutForm.style.display = "none";
+        paymentInfo.style.display = "block";
+        paymentButton.style.display = "block";
+    };
+
+    paymentButton.onclick = () => {
         alert("ORDER RECEIVED!")
-    }
-        
     };
 });
 
 document.addEventListener("DOMContentLoaded", ()=> {
     const form = document.getElementById("checkout-form");
     const button = document.getElementById("confirmOrder");
-
+    button.disabled = true;
     form.addEventListener("input", () => {
         // Check if form is valid if not, enable button
         if (form.checkValidity()){
@@ -180,18 +195,20 @@ const productId = params.get("product");
 const product  = products[productId];
 
 if (product){
+
     document.getElementById("product-image").src = product.image;
     document.getElementById("product-name").textContent = product.name;
     document.getElementById("product-description").textContent = product.description;
     document.getElementById("product-price").textContent = "$" + product.price.toFixed(2);
     
-
     const extrasContainer = document.getElementById("product-extras");
+
     product.extras.forEach(extra => {
         const el = document.createElement("h3");
         el.className = "text-subtitle spacing";
         el.textContent = extra;
         extrasContainer.appendChild(el);
+
     });
 
 
@@ -201,8 +218,10 @@ if (product){
         image: product.image,
         link: product.link,
         id: productId 
+
   });
 } 
+
 
 
 
